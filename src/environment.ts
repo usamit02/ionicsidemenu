244154b89cc4470b0b9c2bd9bd60552020f2f1ee
usamit02 <usamit02@gmail.com>
2019-01-12 09:40:55 +0900

    Update environment.ts

--- src/environment.ts
+++ src/environment.ts
@@ -1,11 +1,6 @@
 import { SocketIoConfig } from 'ng-socket-io';
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAvD0ftnENGOCvE9cOPB8AklV7JeMY4cfg',
-  authDomain: 'blogersguild1.firebaseapp.com',
-  databaseURL: 'https://blogersguild1.firebaseio.com',
-  projectId: 'blogersguild1',
-  storageBucket: 'blogersguild1.appspot.com',
-  messagingSenderId: '1091781872346'
+ 
 };
 //const config: SocketIoConfig = { url: 'http://localhost:3002', options: {} };
-export const config: SocketIoConfig = { url: 'https://www.clife.cf:3002', options: {} };
+export const config: SocketIoConfig = { url: '', options: {} };
